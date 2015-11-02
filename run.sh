f763fac6fd1b9648ddefd8614b0c91d3bea60649
whf <brucewhf@gmail.com>
2015-11-02 13:24:21 +0800

    修改run.sh, 配置好JVM参数

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java $1 -jar target/schdule-center-1.0-SNAPSHOT-executable.jar
+JVM_ARG='-Xms300m -Xmx300m -Xmn160m -XX:+UseG1GC'
+
+if [ "$1" != "" ]; then
+    JVM_ARG=$1
+fi
+
+echo "Using $JVM_ARG"
+
+nohup java $JVM_ARG -jar schdule-center-1.0-SNAPSHOT-executable.jar > /dev/null &
